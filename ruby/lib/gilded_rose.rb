1a6432ca4d7b07edad4719a1e9bc0dafecf78614
Will Howes <williameldenhowes@gmail.com>
2019-08-29 14:38:28 +0100

    Initial tests pass

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,6 +1,9 @@
+require 'item'
+
 class GildedRose
 
   def initialize(items)
+    # DO NOT TOUCH ITEMS PROPERTY
     @items = items
   end
 
@@ -50,19 +53,5 @@ class GildedRose
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
---
+++ ruby/spec/gilded_rose_spec.rb
@@ -0,0 +1,38 @@
+require 'gilded_rose'
+
+describe GildedRose do
+
+  describe "#update_quality" do
+    it "does not change the name" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "foo"
+    end
+
+    it "reduces sell_in by 1" do
+      items = [Item.new("Normal Item", 10, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 9
+    end
+
+    it "reduces quality by 1" do
+      items = [Item.new("Normal Item", 10, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 49
+    end
+
+    it "reduces quality by 2 when 0 days left" do
+      items = [Item.new("Normal Item", 0, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 48
+    end
+
+    it 'increases Aged Brie quality by one' do
+      items = [Item.new("Aged Brie", 10, 20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 21
+    end
+
+  end
+
+end
