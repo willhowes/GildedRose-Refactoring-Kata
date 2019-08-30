0a069bd3152460f162aeed504448bb760475b76a
Will Howes <williameldenhowes@gmail.com>
2019-08-30 10:22:05 +0100

    Added tests for Sulfuras

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -2,41 +2,47 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  let(:account) { double :account }
-
-
   describe "#update_quality" do
 
     context 'Normal item' do
 
+      before(:each) do
+        @item = Item.new("normal", 10, 50)
+      end
+
       it "does not change the name" do
-        items = [Item.new("foo", 0, 0)]
+        items = [@item]
         GildedRose.new(items).update_quality()
-        expect(items[0].name).to eq "foo"
+        expect(items[0].name).to eq "normal"
       end
 
       it "reduces sell_in by 1" do
-        items = [Item.new("Normal Item", 10, 50)]
+        items = [@item]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq 9
       end
 
       it "reduces quality by 1" do
-        items = [Item.new("Normal Item", 10, 50)]
+        items = [@item]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq 49
       end
 
       it "reduces quality by 2 when 0 days left" do
-        items = [Item.new("Normal Item", 0, 50)]
+        items = [Item.new("normal", 0, 50)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq 48
       end
     end
 
     context 'Aged Brie item' do
+
+      before(:each) do
+        @aged_brie_item = Item.new("Aged Brie", 10, 20)
+      end
+
       it 'increases Aged Brie quality by one' do
-        items = [Item.new("Aged Brie", 10, 20)]
+        items = [@aged_brie_item]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq 21
       end
@@ -48,13 +54,29 @@ describe GildedRose do
       end
 
       it 'cannot increase quality above 50' do
-        items = [Item.new("Aged Brie", 0, 20)]
+        items = [@aged_brie_item]
         100.times { GildedRose.new(items).update_quality() }
         expect(items[0].quality).to eq 50
       end
     end
 
     context 'Sulfuras items' do
+
+      before(:each) do
+        @sulfuras_item = Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+      end
+
+      it 'does not change Sulfuras quality' do
+        items = [@sulfuras_item]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 80
+      end
+
+      it 'does not change Sulfuras sell_in date' do
+        items = [@sulfuras_item]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 0
+      end
 
     end
   end
